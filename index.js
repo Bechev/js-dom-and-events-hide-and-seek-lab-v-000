582eac61c5f6cb54f6889e9ca34c4bb31d04c3d9
Bertrand Chevalier <bertrand.chevalier@gmail.com>
2018-08-20 11:01:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ function getFirstSelector(selector){
   return document.querySelector(selector);
 }
 
-
 function nestedTarget(target){
   return document.getElementById("#nested .target")
 }
 
-
 function increaseRankBy(n){
-  
+  let ranks = document.querySelectorAll(".ranked-list")
+
+  for(let i = 0 ; i < ranks.length ; i++){
+    ranks[i].innerHTML = (i+n).toString();
+  }
 }
